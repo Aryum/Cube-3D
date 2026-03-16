c692b45be0f8cd73a1c7a461aa229757495eaf64
Aryum <ricardo.parcelas.santos@gmail.com>
2026-03-16 13:39:47 +0000

    moved things to src folder

--- exercise/src/data/data.h
+++ exercise/src/data/data.h
@@ -1,6 +1,8 @@
 #ifndef DATA_H
 # define DATA_H
 
+#include "../std_header.h"
+
 typedef struct s_vector
 {
 	float x;
@@ -17,5 +19,10 @@ typedef struct s_data
 	t_player	player;
 	char		**map;
 }	t_data;
+
+
+t_data		*data();
+t_player	*player();
+
 
 #endif
---
+++ exercise/src/main/std_header.h
@@ -0,0 +1,9 @@
+#ifndef STD_HEADER_H
+# define STD_HEADER_H
+
+#include <unistd.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#endif
