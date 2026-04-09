b104207400221009d2857ff46e6b657305dc692b
Aryum <ricardo.parcelas.santos@gmail.com>
2026-04-09 17:29:24 +0100

    fixed wall adjustment

--- exercise/src/render/render_cub.c
+++ exercise/src/render/render_cub.c
@@ -1,13 +1,17 @@
 #include "hlp.h"
 
 
-static int set_limit(t_vct i[2], t_vct center, t_vct size)
+static t_vct set_limit(t_vct i[2], t_vct center, t_vct size)
 {
-	float	ret;
+	t_vct	ret;
 
 	i[1] = ini_vct_pos(center.x - size.x / 2.0 , center.y - size.y / 2.0);
 	i[0] = ini_vct_pos(center.x + size.x / 2.0, center.y + size.y / 2.0);
-	ret = i[1].y - i[0].y;
+	ret.x = i[1].y - i[0].y;
+	if(i[1].y < 0)
+		ret.y = -i[1].y;
+	else
+		ret.y = 0;
 	i[0].x = clamp(i[0].x, 0, WINDOW_X - 1);
 	i[0].y = clamp(i[0].y, 0, WINDOW_Y - 1);
 	i[1].x = clamp(i[1].x, 0, WINDOW_X - 1);
@@ -15,10 +19,12 @@ static int set_limit(t_vct i[2], t_vct center, t_vct size)
 	return ret;
 }
 
-static int	get_color(int x, float y, t_frame *f)
+static int	get_color(int x, t_vct i[2], t_vct adjust, t_frame *f)
 {
 	char	*dst;
+	float	y;
 
+	y = (i[1].y - i[0].y - adjust.y) /adjust.x;
 	dst = f->addr + (int)(y * (float)GRIDSIZE) * f->line_len + (x % GRIDSIZE) * f->byte_pixel;
 	return (*(unsigned int *)dst);
 }
@@ -27,10 +33,10 @@ static void	draw(t_vct center, t_vct size, int x, t_frame *texture)
 {
 	t_vct	i[2];
 	t_vct	pivot;
-	float	total;
+	t_vct	adjst;
 	t_frame	*f;
 
-	total = set_limit(i, center, size);
+	adjst = set_limit(i, center, size);
 	f = &render()->frame_buff[render()->frame_cur];
 	pivot.x = i[0].x;
 	while (i[0].y > i[1].y)
@@ -39,7 +45,7 @@ static void	draw(t_vct center, t_vct size, int x, t_frame *texture)
 		pivot.y = (int)i[0].y * f->line_len;
 		while (i[0].x > i[1].x)
 		{
-			set_pixel_pos(i[0].x, i[0].y, get_color(x - 1, (i[1].y - i[0].y) / (total), texture));
+			set_pixel_pos(i[0].x, i[0].y, get_color(x - 1, i, adjst, texture));
 			i[0].x--;
 		}
 		i[0].y--;
